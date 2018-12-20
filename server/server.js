e91e1d73d18d770ad63e285809d8cf86794f8aaf
cinguilherme <cinguilherme@gmail.com>
2018-12-20 11:26:59 -0200

    added get todos to the server

--- server/server.js
+++ server/server.js
@@ -26,6 +26,17 @@ app.post('/todos', (req, res) => {
 
 });
 
+app.get('/todos', (req, res) => {
+
+    Todo.find().then((todos) => {
+
+        res.send({ todos });
+    }, (err) => {
+        res.status(400).send(e);
+    });
+
+});
+
 app.listen(port, () => {
 
     console.log(`app is up on the port ${port}`);
